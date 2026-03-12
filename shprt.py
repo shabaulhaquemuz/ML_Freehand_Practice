03d0c36b8eb2171e09b3d3bdb0ad2cecb0339c45
Shabaul Haque <139956664+shabaulhaquemuz@users.noreply.github.com>
2026-03-12 17:01:40 +0530

    Add files via upload

---
+++ shprt.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+df=pd.read_csv("Covid_toy.csv")
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[4]:
+
+
+df.isnull().sum()
+
+
+# In[5]:
+
+
+from sklearn.impute import SimpleImputer
+si=SimpleImputer()
+df["fever"]=si.fit_transform(df[["fever"]])
+
+
+# In[6]:
+
+
+df.isnull().sum()
+
+
+# In[7]:
+
+
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+df["has_covid"]=le.fit_transform(df["has_covid"])
+
+
+# In[10]:
+
+
+from sklearn.preprocessing import OrdinalEncoder
+oe=OrdinalEncoder()
+df["cough"]=oe.fit_transform(df[["cough"]])
+
+
+# In[11]:
+
+
+df=pd.get_dummies(df,columns=["city","gender"])
+
+
+# In[12]:
+
+
+x=df.drop(columns="has_covid")
+y=df["has_covid"]
+
+
+# In[13]:
+
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2, random_state=42)
+
+
+# In[14]:
+
+
+from sklearn.ensemble import RandomForestClassifier
+rf=RandomForestClassifier()
+rf.fit(x_train,y_train)
+
+
+# In[15]:
+
+
+y_pred=rf.predict(x_test)
+
+
+# In[17]:
+
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+print(accuracy_score(y_test,y_pred))
+print(confusion_matrix(y_test,y_pred))
+
+
+# In[ ]:
+
+
+
+
